166df67cf427a3b47ccfa3ee8c5246f48f11548f
Cmathou <cedric_mathou@hotmail.fr>
2022-02-24 19:09:20 +0100

    fix

--- Project.py
+++ Project.py
@@ -5,10 +5,12 @@ class Project:
     _score = 0
     _deadline = 0
     _roles = 0
+    _skills = []
 
-    def __init__(self, name, length, score, deadline, roles):
+    def __init__(self, name, length, score, deadline, roles, skills):
         _name = name
         _length = length
         _score = score
         _deadline = deadline
         _roles = roles
+        _skills = skills

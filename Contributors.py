fc27e239d10f1067aeb41c94c91321f25e6e2623
Antoine Bralet <bralet@macbook-air-de-antoine.home>
2022-02-24 19:13:01 +0100

    contributor class

---
+++ Contributors.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class Contributor:
+
+    def __init__(self, skills, levels):
+        self.skills = skills
+        self.levels = levels
+    
+    def getLevel(self, skill):
+        if skill in self.skills:
+            a = np.argwhere([s == skill for s in self.skills])[0][0]
+            return self.levels[a]
+        return 0
+    
+    def increaseLevel(self, skill):
+        if not skill in self.skills:
+            self.skills.append(skill)
+            self.levels.append(0)
+        a = np.argwhere([s == skill for s in self.skills])[0][0]  
+        self.levels[a] += 1
+
+
+# t = Contributor(["C++", "python"], [2,4])
+
+# print(t.getLevel("C++"))
+# print(t.getLevel("HTML"))
+# t.increaseLevel("C++")
+
+# print(t.getLevel("C++"))
+
+# t.increaseLevel("HTML")
+
+# print(t.getLevel("HTML"))
+
+
+
+
+

baf110d082a07be0e9fa8c4cff1307fc6b8ca1e4
Cmathou <cedric_mathou@hotmail.fr>
2022-02-24 23:05:20 +0100

    update

--- grid.py
+++ grid.py
@@ -9,18 +9,26 @@ def init(nbFile):
     return T.allProjects, T.allContrib
 
 def isPlacementPossible(grid, day, project_length, contributors_index):
-    for i in range(day, day + project_length):
+    if (day + project_length > last_day):
+        return -1
+    min_available = []
+    for i in range(len(contributors_index)):
+        min_available.append(grid[contributors_index[i]][day:].index(""))
+    
+    max_day = max(min_available)
+    for i in range(max_day, max_day + project_length):
         for j in range(len(contributors_index)):
             if grid[contributors_index[j]][i] != "":
-                return False
-    return True
+                return isPlacementPossible(grid, max_day + 1, project_length, contributors_index)
+            else:
+                return max_day
 
 def setProject(grid, day, project_length, contributors_index, project_name):
     for i in range(day, day + project_length):
         for j in range(len(contributors_index)):
             grid[contributors_index[j]][i] = project_name
 
-nbFile = 2
+nbFile = 4
 last_day = 100
 project_list = []
 contributors_list = []
@@ -28,21 +36,18 @@ contributors_list = []
 projects, contributors = init(nbFile)
 grid = [["" for x in range(last_day)] for y in range(len(contributors))]
 for project in projects:
-    done = False
+    print(projects.index(project)/len(projects))
     contributors_selected = project.searchContributors(contributors)
     if (contributors_selected != []):
-        for day in range(last_day - project.getLength()):
-            if done:
-                continue
-            contributors_index = [contributors.index(x) for x in contributors_selected]
-            if isPlacementPossible(grid, day, project.getLength(), contributors_index):
-                setProject(grid, day, project.getLength(), contributors_index, project.getName())
-                project_list.append(project)
-                contributors_list.append(contributors_selected)
-                done = True
+        contributors_index = [contributors.index(x) for x in contributors_selected]
+        if isPlacementPossible(grid, 0, project.getLength(), contributors_index) >= 0:
+            setProject(grid, 0, project.getLength(), contributors_index, project.getName())
+            project_list.append(project)
+            contributors_list.append(contributors_selected)
+            continue
                 
                 
 
 
 
-writeOutput("c_out.txt", project_list, contributors_list)
+writeOutput("e_out.txt", project_list, contributors_list)

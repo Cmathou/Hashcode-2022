43dd4ff64a11b90a44f2c319d4ab9ad407117868
Cmathou <cedric_mathou@hotmail.fr>
2022-02-24 19:08:21 +0100

    add project class

---
+++ Project.py
@@ -0,0 +1,14 @@
+class Project:
+
+    _name = ""
+    _length = 0
+    _score = 0
+    _deadline = 0
+    _roles = 0
+
+    def __init__(self, name, length, score, deadline, roles):
+        _name = name
+        _length = length
+        _score = score
+        _deadline = deadline
+        _roles = roles

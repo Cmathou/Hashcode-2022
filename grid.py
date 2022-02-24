aa11ea2b00ea860face0e45d5812c216c2c9517b
Cmathou <cedric_mathou@hotmail.fr>
2022-02-24 23:33:49 +0100

    auto size

--- grid.py
+++ grid.py
@@ -13,7 +13,10 @@ def isPlacementPossible(grid, day, project_length, contributors_index):
         return -1
     min_available = []
     for i in range(len(contributors_index)):
-        min_available.append(grid[contributors_index[i]][day:].index(""))
+        if "" in grid[contributors_index[i]][day:]:
+            min_available.append(grid[contributors_index[i]][day:].index(""))
+        else:
+            return -1
     
     max_day = max(min_available)
     for i in range(max_day, max_day + project_length):
@@ -28,15 +31,19 @@ def setProject(grid, day, project_length, contributors_index, project_name):
         for j in range(len(contributors_index)):
             grid[contributors_index[j]][i] = project_name
 
-nbFile = 4
-last_day = 100
+nbFile = 2
 project_list = []
 contributors_list = []
 
 projects, contributors = init(nbFile)
+last_day = 0
+for project in projects:
+    if project.getDeadline() + project.getScore(0) > last_day:
+        last_day = project.getDeadline() + project.getScore(0)
+
 grid = [["" for x in range(last_day)] for y in range(len(contributors))]
 for project in projects:
-    print(projects.index(project)/len(projects))
+    print("{:.2f}".format(100*projects.index(project)/len(projects)))
     contributors_selected = project.searchContributors(contributors)
     if (contributors_selected != []):
         contributors_index = [contributors.index(x) for x in contributors_selected]
@@ -50,4 +57,4 @@ for project in projects:
 
 
 
-writeOutput("e_out.txt", project_list, contributors_list)
+writeOutput("c_out.txt", project_list, contributors_list)

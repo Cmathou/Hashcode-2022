98adeb2de9795c772982819d07010f0d1311d9b8
Bapt-GR <baptiste.gratuze@gmail.com>
2022-02-24 20:21:58 +0100

    outputs

--- Output.py
+++ Output.py
@@ -1,21 +1,25 @@
-class Output:
 
-    _outFile = ["a_out.txt", "b_out.txt", "c_out.txt", "d_out.txt", "e_out.txt", "f_out.txt"]
-    _file = ""
 
-    def __init__(self, fileNbr):
-        self._file = "output/" + self._outFile[fileNbr]
-        with open(self._file, "w+") as file:
-            file.write('0\n')
 
-    def setIntersection(self, intersection, streets, time):
-        with open(self._file, "r") as file:
-            content = file.readlines()
+"""
+line 1 : n of projects
 
-        with open(self._file, "w") as file:
-            content[0] = str(int(content[0]) + 1) + '\n'
-            content.append(str(intersection) + '\n')
-            content.append(str(len(streets)) + '\n')
-            for i in range(len(streets)):
-                content.append(streets[i] + ' ' + str(time[i]) + '\n')
-            file.writelines(content)
+line 2 : name project
+line 3 : contributor 1 / contributor 2 / contributor 3 ...
+
+line 2 : name project
+line 3 : contributor 1 / contributor 2 / contributor 3 ...
+
+...
+
+"""
+
+
+def writeOutput(fileName, projects, contributors)
+    with open(fileName, "w+") as file:
+        file.write(str(len(projects))+"\n")
+
+        for project in range(len(projects)):
+            file.write(projects[project]+"\n")
+            file.write(contributors[project]+"\n")
+
